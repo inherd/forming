48334c33114abf8d7e39ee5dfcd68d82dd0d5094
Phodal Huang <h@phodal.com>
2021-07-03 13:33:04 +0800

    docs: update to refs forming

--- src/bin/forming.rs
+++ src/bin/forming.rs
@@ -1,3 +1,13 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(short, long, default_value = "forming")]
+    input: String,
+}
+
 fn main() {
+    // let opts: Opts = Opts::parse();
     println!("hello, world!");
 }
