43384d68a640339ca77134130aa9f6b9fd510b11
Phodal Huang <h@phodal.com>
2021-09-08 21:26:35 +0800

    feat: init concept ast

---
+++ src/parser/ast.rs
@@ -0,0 +1,60 @@
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ConceptSpace {
+    name: String,
+    package: String,
+    concepts: Vec<Concepts>
+}
+
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Function {
+
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Concepts {
+    name: String,
+    structs: Vec<Struct>,
+    behaviors: Vec<Function>,
+    functions: Vec<Function>
+}
+
+impl Concepts {
+    pub fn new(name: String) -> Concepts {
+        Concepts { name, structs: vec![], behaviors: vec![], functions: vec![] }
+    }
+
+    pub fn from_dir(dir: String) {
+
+    }
+
+    pub fn from_files(path: String) {
+
+    }
+
+    // scan from files
+    pub fn scan() {
+
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Struct {
+
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Behavior {
+
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Type {
+    Int,
+    Float,
+    Double,
+    String,
+    Array,
+    Type
+}
+
