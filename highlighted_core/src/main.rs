85a8c0b855a76f194183d2596a7ec90359f09c3d
Phodal Huang <h@phodal.com>
2021-09-15 20:57:05 +0800

    build: try use guarding api

--- highlighted_core/src/main.rs
+++ highlighted_core/src/main.rs
@@ -1,7 +1,13 @@
+use guarding_ident::ModelBuilder;
+use std::path::PathBuf;
+use std::env;
+
 pub mod document;
 pub mod editor;
 pub mod identifier;
 
 fn main() {
-    println!("Hello, world!");
+    let dir = env::current_dir().unwrap();
+    println!("{:?}", dir.display());
+    let models = ModelBuilder::build_models_by_dir(dir);
 }
