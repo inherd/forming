d0592309935625a0d8efb96a174df558f3f757f6
Phodal Huang <h@phodal.com>
2021-09-17 21:09:18 +0800

    refactor: remove unused comment

---
+++ highlighted_core/src/concept_parser.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
+fn parse_by_dir(filename: PathBuf) {
+
+}
+
+pub fn parse_by_file(filename: PathBuf) {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    parse_csv(contents.as_bytes());
+}
+
+fn parse_csv(data: &[u8]) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(data);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
--- highlighted_core/src/main.rs
+++ highlighted_core/src/main.rs
@@ -1,32 +1,19 @@
-use guarding_ident::ModelBuilder;
-use std::path::PathBuf;
-use std::{env, io, fs};
+use std::{env, fs, io};
 use std::error::Error;
+use std::path::PathBuf;
+
+use guarding_ident::ModelBuilder;
 
 pub mod document;
 pub mod editor;
 pub mod identifier;
-
-fn parse_csv(data: &[u8]) -> Result<(), Box<dyn Error>> {
-    // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_reader(data);
-    for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
+pub mod concept_parser;
 
 fn main() {
     let dir = env::current_dir().unwrap();
 
     let filename = dir.clone().join(".forming").join("concepts.csv");
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    parse_csv(contents.as_bytes());
+    concept_parser::parse_by_file(filename);
 
     let models = ModelBuilder::build_models_by_dir(dir);
 }
