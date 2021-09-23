05bb12b8b2b229d4a37762d845c812302f6ab2b1
Phodal Huang <h@phodal.com>
2021-09-23 08:45:30 +0800

    refactor: inline code

--- writing/src/wmd.rs
+++ writing/src/wmd.rs
@@ -2,10 +2,41 @@ use crate::parser;
 
 use pulldown_cmark::{Event::{Code, End, Start, Text}, Options, Parser, Tag, CodeBlockKind, Event};
 use crate::wreader::WReader;
+use std::borrow::Cow;
 
 pub struct Wmd {
     text: String,
 }
+
+enum Line<'a> {
+    Hidden(&'a str),
+    Shown(Cow<'a, str>),
+}
+
+impl<'a> Line<'a> {
+    fn for_code(self) -> Cow<'a, str> {
+        match self {
+            Line::Shown(l) => l,
+            Line::Hidden(l) => Cow::Borrowed(l),
+        }
+    }
+}
+
+fn map_line(s: &str) -> Line<'_> {
+    let trimmed = s.trim();
+    if trimmed.starts_with("##") {
+        Line::Shown(Cow::Owned(s.replacen("##", "#", 1)))
+    } else if let Some(stripped) = trimmed.strip_prefix("# ") {
+        // # text
+        Line::Hidden(&stripped)
+    } else if trimmed == "#" {
+        // We cannot handle '#text' because it could be #[attr].
+        Line::Hidden("")
+    } else {
+        Line::Shown(Cow::Borrowed(s))
+    }
+}
+
 
 impl Wmd {
     pub fn new(text: String) -> Wmd {
@@ -13,29 +44,33 @@ impl Wmd {
     }
 
     pub fn parse(&mut self) -> String {
-        let mut parser = create_markdown_parser(&self.text).into_offset_iter();
+        let doc = &self.text;
+        let mut parser = create_markdown_parser(doc).into_offset_iter();
         let mut text = "".to_string();
-        let mut is_in_code = false;
         let mut lang_code = "".to_string();
 
         while let Some((event, _offset)) = parser.next() {
             match event {
-                Start(Tag::CodeBlock(info)) => {
+                Event::Start(Tag::CodeBlock(info)) => {
                     match info {
                         CodeBlockKind::Fenced(lang) => {
                             lang_code = format!("{}", lang);
                         }
                         CodeBlockKind::Indented => {}
                     }
-                    is_in_code = true;
-                }
-                End(Tag::CodeBlock(_info)) => {
-                    is_in_code = false;
-                }
-                Text(body) => {
-                    let str = body.to_string();
 
-                    if is_in_code && str.starts_with("// doc-") {
+                    let mut test_s = String::new();
+
+                    while let Some((Event::Text(s), _)) = parser.next() {
+                        test_s.push_str(&s);
+                    }
+                    let str = test_s
+                        .lines()
+                        .map(|l| map_line(l).for_code())
+                        .collect::<Vec<Cow<'_, str>>>()
+                        .join("\n");
+
+                    if str.starts_with("// doc-") {
                         let writing = parser::parse(str.replace("//", "").as_str());
                         let result = WReader::read_doc_code(writing.code_docs[0].clone());
 
@@ -48,7 +83,13 @@ impl Wmd {
                         text += &format!("{}\n", str);
                     }
                 }
-                Code(inline_code) => {
+                Event::End(Tag::CodeBlock(_info)) => {
+                }
+                Event::Text(body) => {
+                    let str = body.to_string();
+                    text += &format!("{}\n", str);
+                }
+                Event::Code(inline_code) => {
                     text += &format!("`{}`", inline_code);
                 }
                 Event::Html(html) => {
