35df3be08f5625e8e5a6f98a0e4db39ad984f139
Phodal Huang <h@phodal.com>
2021-10-20 23:35:52 +0800

    refactor: change to origin file replace

--- writing/src/lib.rs
+++ writing/src/lib.rs
@@ -3,10 +3,13 @@ extern crate pest;
 extern crate pest_derive;
 
 use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 
-use pulldown_cmark::{Options, Parser};
 use thiserror::Error;
+
+use crate::md_reader::CodeReader;
 
 pub mod parser;
 pub mod md_reader;
@@ -25,41 +28,53 @@ pub enum WritingError {
 pub struct Writing {}
 
 impl Writing {
-    pub fn process_file<P: AsRef<Path>>(path: P)  -> Result<String, WritingError> {
+    pub fn process_file<P: AsRef<Path>>(path: P) -> Result<String, WritingError> {
         let path = path.as_ref().to_path_buf();
 
-        let blob = match Writing::pre_process_file(&path) {
-            Ok(s) => s,
-            Err(err) => {
-                return Err(err);
-            }
+        if let Err(err) = Writing::pre_process_file(&path) {
+            return Err(err);
         };
 
-        let text = String::from_utf8_lossy(&blob).to_string();
-
-        let parser = Parser::new_ext(&*text, Options::all());
-        let mut result: String = String::from("");
-
-        md_writer::push_text(&mut result, parser);
+        let result = Writing::write_it(path).join("\n");
 
         Ok(result)
     }
 
-    fn pre_process_file(path: &PathBuf) -> Result<Vec<u8>, WritingError> {
+    fn write_it(path: PathBuf) -> Vec<String> {
+        let file = File::open(path).expect("cannot open file");
+        let reader = BufReader::new(file);
+
+        let mut results =  vec![] ;
+        for res in reader.lines() {
+            let line = res.expect("cannot parse line");
+            if line.starts_with("// doc-") {
+                let writing = parser::parse(line.replace("//", "").as_str());
+
+                if writing.code_docs.len() > 0 {
+                    results.append(&mut CodeReader::read_doc_code(&writing.code_docs[0]));
+                } else if writing.code_sections.len() > 0 {
+                    results.append(&mut CodeReader::read_doc_section(&writing.code_sections[0]));
+                } else {
+                    results.push(String::from(line));
+                };
+            } else {
+                results.push(String::from(line));
+            }
+        }
+
+        results
+    }
+
+    fn pre_process_file(path: &PathBuf) -> Result<(), WritingError> {
         if path.is_dir() {
-            return Err(WritingError::IOError(format!("path: {:?} is a dir", path)))
+            return Err(WritingError::IOError(format!("path: {:?} is a dir", path)));
         }
 
         if let Err(e) = fs::read(path) {
-            return Err(WritingError::IOError(format!("read file error: {:?}", e)))
+            return Err(WritingError::IOError(format!("read file error: {:?}", e)));
         }
 
-        let blob = match fs::read(&path) {
-            Ok(s) => s,
-            Err(e) => return Err(WritingError::ReadFileError(format!("error: {:?}", &e))),
-        };
-
-        Ok(blob)
+        Ok(())
     }
 }
 
